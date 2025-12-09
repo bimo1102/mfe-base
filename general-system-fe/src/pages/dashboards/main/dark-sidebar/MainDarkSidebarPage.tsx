6127c7420c4c34a4f54a8c0628f25cbe3f9f2cc3
lamdev <levanlam270920@gmail.com>
2025-12-09 14:47:13 +0700

    update

--- general-system-fe/src/pages/dashboards/main/dark-sidebar/MainDarkSidebarPage.tsx
+++ general-system-fe/src/pages/dashboards/main/dark-sidebar/MainDarkSidebarPage.tsx
@@ -1,17 +1,9 @@
 import { Container } from '@/components/container';
 import { Toolbar, ToolbarActions, ToolbarHeading } from '@/layouts/mains/toolbar';
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import { MainLightSidebarContent } from '../light-sidebar';
 
-import { addDays } from 'date-fns';
-import { DateRange } from 'react-day-picker';
-
 const MainDarkSidebarPage = () => {
-    const [date, setDate] = useState<DateRange | undefined>({
-        from: new Date(2025, 0, 20),
-        to: addDays(new Date(2025, 0, 20), 20),
-    });
-
     return (
         <Fragment>
             <Container>
